4de9b2a0cab6d38507430e2d6c9c7611f26f3c66
NekoM200 <lychees67@gmail.com>
2022-09-17 18:33:05 +0800

    go

---
+++ docs/src/Ch-1 快速入门/counter.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Counter {
+    uint public count;
+
+    // Function to get the current count
+    function get() public view returns (uint) {
+        return count;
+    }
+
+    // Function to increment count by 1
+    function inc() public {
+        count += 1;
+    }
+}
